3aa881809eec0c1aac8f01cade372cb3871a5f0b
Diovana Manica <dhiovana_pires@hotmail.com>
2024-08-05 20:14:17 -0300

    Entrega Prova

---
+++ Prova/script.js
@@ -0,0 +1,10 @@
+function nome_capturado_input () { 
+    nome_capturado_input = "Nome"
+}
+function email_capturado_input () {
+    email_capturado_input = "Email"
+}
+
+
+<div id="mensagem_confirmacao">Muito Obrigado(a), senhor(a) [Nome]. 
+    Acabamos de enviar um e-mail para: [Email] </div>
